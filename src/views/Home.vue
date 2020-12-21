64c415115332361c07ce9fd3115d2ac85c29eb9a
muhsinahadi <muhsinahadi25@gmail.com>
2020-12-21 16:12:52 +0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <MyHeader></MyHeader>
+    <MyCarousel></MyCarousel>
+    <WomanCarousel></WomanCarousel>
+    <GalleryInstagram></GalleryInstagram>
+    <partner-logo></partner-logo>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyHeader from '@/components/Home/Header'
+import MyCarousel from '@/components/Home/Carousel'
+import WomanCarousel from '@/components/Home/Carousel-Women'
+import GalleryInstagram from '@/components/Home/Gallery'
+import PartnerLogo from '@/components/Home/PartnerLogo'
+import MyFooter from '@/components/Home/Footer'
+export default {
+  name: 'Home',
+  components: {
+    MyHeader,
+    MyCarousel,
+    WomanCarousel,
+    GalleryInstagram,
+    PartnerLogo,
+    MyFooter
+  }
+}
+</script>
